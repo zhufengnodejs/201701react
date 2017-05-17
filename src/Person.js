9b1f62fc119ddc691176c70be2adae2d2d7df5d4
zhufengnodejs <zhufengnodejs@126.com>
2017-05-17 15:19:38 +0800

    hello

---
+++ src/Person.js
@@ -0,0 +1,14 @@
+import React from 'react';
+//Person.say();
+//new Person().render();
+export default class Person extends React.Component{
+    static say(){}
+    render(){
+        return (
+            <div>
+                <p>姓名:{this.props.name}</p>
+                <p>年龄:{this.props.age}</p>
+            </div>
+        )
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
-import Panel from  './Panel.js';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Person from  './Person.js';
 //  {name:"zfpx",age:"9"}
 //自己实现一个面板组件
-ReactDOM.render(<Panel type="danger" head="头" body="体" footer="尾"></Panel>,document.getElementById('root'));
+/**
+ * 渲染一个组件的时候
+ * 1. 先去实例化这个组件类
+ * 2. 调用组件实例的render方法返回一个react元素
+ */
+/*let p = new Person();
+p.props = {name:"zfpx",age:"9"};
+let ele = p.render();*/
+
+ReactDOM.render(<Person name="zfpx" age="8" ></Person>,document.getElementById('root'));
 
 
 
