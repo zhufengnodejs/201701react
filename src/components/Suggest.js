9afbbbe0bdf6505e36762f8ecb09332658e3ace2
zhufengnodejs <zhufengnodejs@126.com>
2017-05-18 16:06:09 +0800

    hello

--- src/components/Suggest.js
+++ src/components/Suggest.js
@@ -5,42 +5,56 @@ import $ from 'jquery';
  * 2. 获得输入框的值，然后调用百度的接口获取联想数组
  * 3. 把数组赋给状态对象的words属性
  */
-export default class Suggest extends React.Component{
-    constructor(props){
+export default class Suggest extends React.Component {
+    constructor(props) {
         super(props);
-        this.state = {words:[]};
+        this.state = {wd:'',words: [],pos:-1};
     }
-    handleChange = (event)=>{
+
+    handleChange = (event) => {
         let wd = event.target.value;
+        this.setState({wd});
         // 1.success
         $.ajax({
-            type:'GET',//请求的方法为GET
-            url:`http://www.baidu.com/su`,
-            data:{wd},//此对象会转成查询字符串放在url后面
-            jsonp:'cb',//指定参数名 cb=方法名
-            dataType:'jsonp',//指定返回值的类型
-            success:(result)=>{
+            type: 'GET',//请求的方法为GET
+            url: `http://www.baidu.com/su`,
+            data: {wd},//此对象会转成查询字符串放在url后面
+            jsonp: 'cb',//指定参数名 cb=方法名
+            dataType: 'jsonp',//指定返回值的类型
+            success: (result) => {
                 this.setState({
-                    words:result.s
+                    words: result.s
                 });
             }
         });
 
     }
-    render(){
+    handleKeyDown = (event)=>{
+        let code = event.keyCode;
+        if(code == 38 || code == 40){
+            let pos = this.state.pos;
+            if(code == 38){//向上
+                pos--;
+            }else if(code == 40){//向下
+                pos++;
+            }
+            this.setState({pos,wd:this.state.words[pos]});
+        }
+    }
+    render() {
         return (
-            <div style={{marginTop:30}} className="container">
+            <div style={{marginTop: 30}} className="container">
                 <div className="row">
                     <div className="col-md-6 col-md-offset-3">
-                        <input type="text" className="form-control" onChange={this.handleChange}/>
+                        <input type="text" className="form-control" value={this.state.wd} onKeyDown={this.handleKeyDown} onChange={this.handleChange}/>
                     </div>
                 </div>
                 <div className="row">
                     <div className="col-md-6 col-md-offset-3">
                         <ul className="list-group">
                             {
-                                this.state.words.map((word,index)=>(
-                                    <li className="list-group-item" key={index}>{word}</li>
+                                this.state.words.map((word, index) => (
+                                    <li className={"list-group-item "+(this.state.pos==index?'active':'')} key={index}>{word}</li>
                                 ))
                             }
                         </ul>
