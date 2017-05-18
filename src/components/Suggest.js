c56a0795f049550ea5cc59eda79bf88834899240
zhufengnodejs <zhufengnodejs@126.com>
2017-05-18 15:36:12 +0800

    hello

--- src/components/Suggest.js
+++ src/components/Suggest.js
@@ -10,18 +10,39 @@ export default class Suggest extends React.Component{
         super(props);
         this.state = {words:[]};
     }
+    handleChange = (event)=>{
+        let wd = event.target.value;
+        // 1.success
+        $.ajax({
+            type:'GET',//请求的方法为GET
+            url:`http://www.baidu.com/su`,
+            data:{wd},//此对象会转成查询字符串放在url后面
+            jsonp:'cb',//指定参数名 cb=方法名
+            dataType:'jsonp',//指定返回值的类型
+            success:(result)=>{
+                this.setState({
+                    words:result.s
+                });
+            }
+        });
+
+    }
     render(){
         return (
             <div style={{marginTop:30}} className="container">
                 <div className="row">
                     <div className="col-md-6 col-md-offset-3">
-                        <input type="text" className="form-control"/>
+                        <input type="text" className="form-control" onChange={this.handleChange}/>
                     </div>
                 </div>
                 <div className="row">
                     <div className="col-md-6 col-md-offset-3">
                         <ul className="list-group">
-                            <li className="list-group-item">Airbnb</li>
+                            {
+                                this.state.words.map((word,index)=>(
+                                    <li className="list-group-item" key={index}>{word}</li>
+                                ))
+                            }
                         </ul>
                     </div>
                 </div>
